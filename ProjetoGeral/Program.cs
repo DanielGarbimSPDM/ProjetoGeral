f74fe6b9b8306a57ac52e0716707e6d5dfb87a8d
Daniel <daniel.garbim.spdm@hotmail.com>
2023-10-08 19:55:26 -0300

    Add project files.

---
+++ ProjetoGeral/Program.cs
@@ -0,0 +1,8 @@
+﻿// Aqui vai ser onde vamos criar os objetos e testar os metodos e tudo mais
+
+using ProjetoGeral;
+
+Automovel celta = new Automovel();
+celta.tipo = "Carro";
+
+Console.WriteLine(celta.tipo);
